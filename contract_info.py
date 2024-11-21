791bf36df234cca57df1ff30ae3ad3f099883a4b
Choi Dong-bin <43774570+DBChoi85@users.noreply.github.com>
2024-11-21 19:04:56 +0900

    Create contract_info.py

---
+++ contract_info.py
@@ -0,0 +1,47 @@
+contract_addr = '0x761A6092604C5990D84C18cD488b99509aaAF64f'
+abi = [
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "num",
+				"type": "uint256"
+			}
+		],
+		"name": "store",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"inputs": [],
+		"name": "owner",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "retrieve",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+]
